fdd4cfa80660592f704f69d9414557deb22daf17
oguzhanntuna <oguzhana.tuna@gmail.com>
2021-03-15 23:40:50 +0300

    sign up form submit handler and authValidation set up

--- src/components/UI/Input/Input.tsx
+++ src/components/UI/Input/Input.tsx
@@ -5,11 +5,12 @@ import './Input.scss';
 interface InputProps {
     type: string;
     id: string;
+    referance: any;
 }
 
 const Input: React.FC<InputProps> = (props) => {
     return (
-        <input className="form-input" type={props.type} id={props.id} />
+        <input className="form-input" type={props.type} id={props.id} ref={props.referance}/>
     );
 }
 
--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,28 +1,105 @@
-import React from 'react';
+import React, { useState, useRef , useEffect } from 'react';
+
+import axios from 'axios';
 
 import './SignUp.scss';
 import Button from '../../components/UI/Button/Button';
 import Input from '../../components/UI/Input/Input';
 
+interface AuthenticationInfo {
+    name: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+}
+
 const SignUp: React.FC = () => {
-    const signupSubmitHandler = (event: React.FormEvent) => {
+    const [authenticationInfo, setAuthenticationInfo] 
+        = useState<AuthenticationInfo>({name: '', email: '', password: '', confirmPassword: ''});
+    const [validation, setValidation] = useState<boolean>(false);
+
+    let nameRef = useRef<HTMLInputElement>(null);
+    let emailRef = useRef<HTMLInputElement>(null);
+    let passwordRef = useRef<HTMLInputElement>(null);
+    let confirmPasswordRef = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+        if (validation) {
+            setAuthenticationInfo({
+                name: nameRef.current!.value,
+                email: emailRef.current!.value,
+                password: passwordRef.current!.value,
+                confirmPassword: confirmPasswordRef.current!.value
+            }); 
+            nameRef.current!.value = '';
+            emailRef.current!.value = '';
+            passwordRef.current!.value = '';
+            confirmPasswordRef.current!.value = '';
+        }
+    }, [validation]);
+
+    const signupSubmitHandler = (event: React.FormEvent): void => {
         event.preventDefault();
+        AuthenticationValidation(nameRef.current!.value, emailRef.current!.value, passwordRef.current!.value, confirmPasswordRef.current!.value);
     }
+
+    const AuthenticationValidation = (
+        name: string, 
+        email: string, 
+        password: string, 
+        confirmPassword: string
+    ) => {
+        if (name === '' || email === '' || password === '' || confirmPassword === '') {
+            alert('Fill out the empty spaces!');
+        } else {
+            if (password === confirmPassword) {
+                setValidation(true);
+            } else {
+                alert('Passwords do not match!');
+            }
+        }
+    }
+
+    useEffect(() => {
+        console.log(authenticationInfo);
+    }, [authenticationInfo]);
+
+    /*
+    const authentication = () => {
+        axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDq_Nhd-h7EudYW4skt0UozTwSD1GF0qcI`)
+    }
+    */
 
     return (
         <div className="signup">
             <form className="signup-form" onSubmit={signupSubmitHandler}>
                 <div className="signup-form-fullName">
-                    <label htmlFor="fullname">Full name:</label>
-                    <Input type="text" id="fullname" />
+                    <label htmlFor="signup-fullname">Name:</label>
+                    <Input 
+                        type="text" 
+                        id="signup-fullname" 
+                        referance={nameRef} />
+                </div>
+                <div className="signup-form-email">
+                    <label htmlFor="signup-email">Email:</label>
+                    <Input 
+                        type="email" 
+                        id="signup-email" 
+                        referance={emailRef} />
                 </div>
                 <div className="signup-form-password">
-                    <label htmlFor="password">Password:</label>
-                    <Input type="text" id="password" />
+                    <label htmlFor="signup-password">Password:</label>
+                    <Input 
+                        type="text" 
+                        id="signup-password" 
+                        referance={passwordRef} />
                 </div>
                 <div className="signup-form-confirmPassword">
-                    <label htmlFor="password-again">Confirm Password:</label>
-                    <Input type="text" id="password-confirm" />
+                    <label htmlFor="signup-confirmPassword">Confirm Password:</label>
+                    <Input 
+                        type="text" 
+                        id="signup-confirmPassword" 
+                        referance={confirmPasswordRef} />
                 </div> 
                 <Button className="signup-form-button">Sign up</Button>
             </form>
